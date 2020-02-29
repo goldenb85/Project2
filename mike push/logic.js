e7eb1e72ec7a1d64f09f091e33f29dfb509b278d
Michael Becker <becker1855@yahoo.com>
2020-02-29 11:57:58 -0600

    website

---
+++ mike push/logic.js
@@ -0,0 +1,16 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+  center: [37.60, -95.6650],
+  zoom: 5
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: "pk.eyJ1IjoiYmVja2VyMTg1NSIsImEiOiJjazZqejRlZWEwMWZiM211NzZlcm1tN3E4In0.Qb0GdJabB3l1G6WKnyccyA"
+}).addTo(myMap);
