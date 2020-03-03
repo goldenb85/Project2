9bce96a75a8254ff2eced7cbea0f5c9e39ec82f0
MeagColley <meagcolley@gmail.com>
2020-03-02 21:45:43 -0600

    the last charts

--- project_working/static/dumbell_logic.js
+++ project_working/static/dumbell_logic.js
@@ -1,4 +1,71 @@
-var stateData = "data/dumbell_logic.js";
+var stateData = data
+
+console.log(stateData)
+
+
+
+
+
+
+var data = [{
+    "name": "Ohio",
+    "high": 11689442,
+    "low": 11594163
+}, {
+    "name": "Washington",
+    "high": 7535591,
+    "low": 7061530
+}, {
+    "name": "Virginia",
+    "high": 8517685,
+    "low": 8326289
+}, {
+    "name": "California",
+    "high": 39557045,
+    "low": 38802500
+}, {
+    "name": "Texas",
+    "high": 28701845,
+    "low": 26956958
+}, {
+    "name": "Pennsylvania",
+    "high": 12807060,
+    "low": 12787209
+}, {
+    "name": "Florida",
+    "high": 21299325,
+    "low": 19893297
+}, {
+    "name": "Georgia",
+    "high": 10519475,
+    "low": 10097343
+}, {
+    "name": "North Carolina",
+    "high": 10383620,
+    "low": 9943964
+}, {
+    "name": "New York",
+    "high": 19542209,
+    "low": 19746227
+}, {
+    "name": "Illinois",
+    "high": 12741080,
+    "low": 12880580
+}, {
+    "name": "New Jersey",
+    "high": 8908520,
+    "low": 8938175
+}, {
+    "name": "Michigan",
+    "high": 9995915,
+    "low": 9909877
+}]
+
+
+
+
+
+
 
 Highcharts.chart('container', {
 
@@ -29,13 +96,16 @@ Highcharts.chart('container', {
 
     yAxis: {
         title: {
-            text: 'State Population Change'
+            text: 'State Population'
         }
     },
 
     series: [{
-        name: 'State Population Change',
-        data: stateData
+        name: 'State Population',
+        data: data
     }]
 
 });
+
+
+// end new stuff 
